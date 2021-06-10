b4437ba5505cd73affe53b04cb448dfd4545ab2c
Diego Costa <diego.szct@gmail.com>
2021-06-10 18:27:14 -0300

    Feat(directory_server): Add messages per requests

--- directory/directory_server.py
+++ directory/directory_server.py
@@ -1,6 +1,8 @@
 from rpyc import Service
 from rpyc.utils.server import ThreadedServer
 import sys
+from datetime import datetime
+
 from constRPYC import DIR_SERVER_IP, DIR_SERVER_PORT
 
 _SERVER = "SERVER"
@@ -12,6 +14,7 @@ class Directory(Service):
     registry_users = dict()
 
     def exposed_register_server(self, ip_adress, port_number):
+        print(f"[{datetime.now()}] - Registering Server")
         
         self.registry_server[_SERVER] = {
             _IP: ip_adress,
@@ -21,6 +24,7 @@ class Directory(Service):
         return True
 
     def exposed_retrieve_server(self):
+        print(f"[{datetime.now()}] - Retrieving Server")
 
         if _SERVER in self.registry_server.keys():
             return self.registry_server[_SERVER]
@@ -28,6 +32,8 @@ class Directory(Service):
         return None
 
     def exposed_register_user(self, username, ip_adress, port_number):
+        print(f"[{datetime.now()}] - Registering new user")
+
         self.registry_users[username] = {
             _IP: ip_adress,
             _PORT: port_number
@@ -36,25 +42,31 @@ class Directory(Service):
         return True
 
     def exposed_retrieve_user(self, username):
+        print(f"[{datetime.now()}] - Retrieving user")
+
         if username in self.registry_users.keys():
             return self.registry_users[username]
         
         return None
 
     def exposed_check_user(self, username):
+        print(f"[{datetime.now()}] - Checking user")
+
         if username in self.registry_users.keys():
             return True
         
         return False
 
     def exposed_list_users(self):
+        print(f"[{datetime.now()}] - Listing users")
+
         return list(self.registry_users.keys())
 
 
 def run():
     try:
         server_dir = ThreadedServer(Directory, port = DIR_SERVER_PORT )
-        print ("Server Started. Waiting Requests")
+        print ("Directory Server Started. Waiting Requests")
         server_dir.start()
 
     finally:
